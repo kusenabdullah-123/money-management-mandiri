84073d7303e5f0bdd03080f8e710350798a36fc6
M Kusen Abdullah <kusena322@gmail.com>
2021-02-05 10:12:31 +0700

    partial file

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const app = express();
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+const url = `http://localhost:${process.env.PORT}`;
 app.get("/", (req, res) => {
   res.render("login");
 });
+app.get("/admin/dashboard", (req, res) => {
+  res.render("admin/dashboard", { url });
+});
+app.get("/admin/kas", (req, res) => {
+  res.render("admin/kas/", { url });
+});
 app.listen(process.env.PORT, () => {
-  console.log(`http://localhost:${process.env.PORT}`);
+  console.log(url);
 });
